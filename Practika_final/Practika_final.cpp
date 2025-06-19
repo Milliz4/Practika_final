a8ea71a8c60b1d4c306f4e9bf91fb99c3f78d34f
Алина Бочкарникова <alinabochkarnikova@gmail.com>
2025-06-19 14:37:26 +0800

    пробный неработающий код

--- Practika_final/Practika_final.cpp
+++ Practika_final/Practika_final.cpp
@@ -1,76 +1,107 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 #include <windows.h>
 
 using namespace std;
 
-// Функция для преобразования русских букв
-wchar_t transformChar(wchar_t c, int shift, bool encrypt) {
-    if (c >= L'А' && c <= L'Я') {
-        if (encrypt) return L'А' + (c - L'А' + shift) % 32;
-        else return L'А' + (c - L'А' - shift + 32) % 32;
+// Русский алфавит в UTF-8 (первая часть двухбайтовых последовательностей)
+const vector<pair<unsigned char, unsigned char>> RUS_UTF8 = {
+    {0xD0, 0x90}, {0xD0, 0x91}, {0xD0, 0x92}, {0xD0, 0x93}, {0xD0, 0x94},
+    {0xD0, 0x95}, {0xD0, 0x81}, {0xD0, 0x96}, {0xD0, 0x97}, {0xD0, 0x98},
+    {0xD0, 0x99}, {0xD0, 0x9A}, {0xD0, 0x9B}, {0xD0, 0x9C}, {0xD0, 0x9D},
+    {0xD0, 0x9E}, {0xD0, 0x9F}, {0xD0, 0xA0}, {0xD0, 0xA1}, {0xD0, 0xA2},
+    {0xD0, 0xA3}, {0xD0, 0xA4}, {0xD0, 0xA5}, {0xD0, 0xA6}, {0xD0, 0xA7},
+    {0xD0, 0xA8}, {0xD0, 0xA9}, {0xD0, 0xAA}, {0xD0, 0xAB}, {0xD0, 0xAC},
+    {0xD0, 0xAD}, {0xD0, 0xAE}, {0xD0, 0xAF}, {0xD0, 0xB0}, {0xD0, 0xB1},
+    {0xD0, 0xB2}, {0xD0, 0xB3}, {0xD0, 0xB4}, {0xD0, 0xB5}, {0xD1, 0x91},
+    {0xD0, 0xB6}, {0xD0, 0xB7}, {0xD0, 0xB8}, {0xD0, 0xB9}, {0xD0, 0xBA},
+    {0xD0, 0xBB}, {0xD0, 0xBC}, {0xD0, 0xBD}, {0xD0, 0xBE}, {0xD0, 0xBF},
+    {0xD1, 0x80}, {0xD1, 0x81}, {0xD1, 0x82}, {0xD1, 0x83}, {0xD1, 0x84},
+    {0xD1, 0x85}, {0xD1, 0x86}, {0xD1, 0x87}, {0xD1, 0x88}, {0xD1, 0x89},
+    {0xD1, 0x8A}, {0xD1, 0x8B}, {0xD1, 0x8C}, {0xD1, 0x8D}, {0xD1, 0x8E},
+    {0xD1, 0x8F}
+};
+
+// Поиск позиции символа в алфавите
+int findPos(unsigned char c1, unsigned char c2) {
+    for (int i = 0; i < RUS_UTF8.size(); ++i) {
+        if (RUS_UTF8[i].first == c1 && RUS_UTF8[i].second == c2)
+            return i;
     }
-    else if (c >= L'а' && c <= L'я') {
-        if (encrypt) return L'а' + (c - L'а' + shift) % 32;
-        else return L'а' + (c - L'а' - shift + 32) % 32;
-    }
-    return c; // Оставляем другие символы без изменений
+    return -1;
 }
 
-// Функция обработки файла
-void processFile(const wstring& inputFile, const wstring& outputFile, int shift, bool encrypt) {
-    wifstream inFile(inputFile);
-    inFile.imbue(locale("rus_rus.866"));
+// Функция шифрования
+string encrypt(const string& text, int key) {
+    string result;
+    int size = RUS_UTF8.size();
 
-    if (!inFile) {
-        wcerr << L"Ошибка открытия входного файла" << endl;
-        return;
+    for (size_t i = 0; i < text.size(); ) {
+        // Обрабатываем UTF-8 символы (русские буквы - 2 байта)
+        if ((text[i] & 0xE0) == 0xC0 && i + 1 < text.size()) {
+            int pos = findPos(text[i], text[i + 1]);
+            if (pos != -1) {
+                int newPos = (pos + key + size) % size;
+                result += RUS_UTF8[newPos].second;
+                i += 2;
+                continue;
+            }
+        }
+        // Все остальные символы оставляем как есть
+        result += text[i];
+        i++;
     }
+    return result;
+}
 
-    wofstream outFile(outputFile);
-    outFile.imbue(locale("rus_rus.866"));
+// Чтение файла как бинарного
+string readFile(const string& filename) {
+    ifstream file(filename, ios::binary);
+    return string((istreambuf_iterator<char>(file)),
+        istreambuf_iterator<char>());
+}
 
-    wchar_t c;
-    while (inFile.get(c)) {
-        outFile.put(transformChar(c, shift, encrypt));
-    }
-
-    inFile.close();
-    outFile.close();
+// Запись файла с принудительным UTF-8
+void writeFile(const string& filename, const string& content) {
+    ofstream file(filename, ios::binary);
+    // Добавляем BOM для явного указания UTF-8
+    file << "\xEF\xBB\xBF";
+    file << content;
 }
 
 int main(int argc, char* argv[]) {
-    // Настройка локали для поддержки русского языка
-    SetConsoleCP(1251);
-    SetConsoleOutputCP(1251);
-    setlocale(LC_ALL, "Russian");
+    if (argc < 2) {
+        cout << "Usage:\n"
+            << "  cipher_force_utf8.exe input.txt output.txt -e -k 3\n"
+            << "  cipher_force_utf8.exe encrypted.txt decrypted.txt -d -k 3\n";
+        return 0;
+    }
 
-    // Проверка аргументов
-    if (argc != 5) {
-        wcout << L"Использование:\n"
-            << L"Шифрование: Practika_final.exe входной_файл выходной_файл сдвиг encrypt\n"
-            << L"Дешифровка: Practika_final.exe входной_файл выходной_файл сдвиг decrypt\n"
-            << L"Пример: Practika_final.exe text.txt encrypted.txt 5 encrypt\n";
-        return 1;
+    string inputFile, outputFile;
+    int key = 3;
+
+
+    // Обработка аргументов
+    for (int i = 1; i < argc; ++i) {
+        string arg = argv[i];
+        if (arg == "-e") encryptMode = true;
+        else if (arg == "-d") encryptMode = false;
+        else if (arg == "-k" && i + 1 < argc) key = stoi(argv[++i]);
+        else if (inputFile.empty()) inputFile = arg;
+        else if (outputFile.empty()) outputFile = arg;
     }
 
     try {
-        // Преобразование аргументов в wide-строки
-        wstring inputFile(argv[1], argv[1] + strlen(argv[1]));
-        wstring outputFile(argv[2], argv[2] + strlen(argv[2]));
-        int shift = stoi(argv[3]);
-        string mode = argv[4];
+        string text = readFile(inputFile);
+        string result = encrypt(text, encryptMode ? key : -key);
 
-        if (mode != "encrypt" && mode != "decrypt") {
-            throw runtime_error("Неверный режим работы");
-        }
-
-        processFile(inputFile, outputFile, shift, mode == "encrypt");
-        wcout << L"Операция выполнена успешно!\n";
+        writeFile(outputFile, result);
+        cout << "File successfully processed: " << outputFile << endl;
     }
     catch (const exception& e) {
-        wcerr << L"Ошибка: " << e.what() << endl;
+        cerr << "Error: " << e.what() << endl;
         return 1;
     }
 
